43b6fb45f07068016f450a8e44abcae7b526c746
Dan Constantinescu <dan_constantinescu@outlook.com>
2016-12-23 10:02:44 +0200

    Routing

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,23 @@
 <template>
   <div class="container" id="app">
     <navbar> </navbar>
-    <login> </login>
-    <register> </register>
+    <router-view> </router-view>
+    <!--<login> </login>
+    <register> </register>-->
   </div>
 </template>
 
 <script>
 
   import Navbar from './components/Navbar.vue'
-  import Login from './components/authentication/Login.vue'
-  import Register from './components/authentication/Register.vue'
+  //import Login from './components/authentication/Login.vue'
+ // import Register from './components/authentication/Register.vue'
 
   export default {
     components: {
       'navbar': Navbar,
-      'login': Login,
-      'register': Register
+      //'login': Login,
+      //'register': Register
     }
   }
 
